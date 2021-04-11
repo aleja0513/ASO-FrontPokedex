807e00a83939a864edbff60020bb3c7aa760bf3d
aleja0513 <aleja0513@gmail.com>
2021-04-11 11:45:33 -0500

    Configuring prod deploy

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/aso-frontpokedex'));
+app.use(express.static(__dirname + '/dist/ASO-FrontPokedex'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/aso-frontpokedex/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/ASO-FrontPokedex/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
